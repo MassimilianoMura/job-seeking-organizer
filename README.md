0b6550e5bd642326b15e6742bd082710aa71ee88
MassimilianoMura <massimilianomura1983@gmail.com>
2015-11-30 17:42:28 +0000

    adds readme file and unique username and  email code

---
+++ README.md
@@ -0,0 +1,5 @@
+##job seeking organizer app
+
+####WORN IN PROGRESS
+
+a full stack js project, using passport for user authentication
--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -3,8 +3,14 @@ var mongoose = require('mongoose'),
 
 var UserSchema = new Schema({
     name: String,
-    email: String,
-    username: String,
+    email: {
+            type: String,
+            unique: true
+            },
+    username: {
+                type: String,
+                unique: true
+              },
     password: String
 });
 
