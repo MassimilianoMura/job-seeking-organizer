f25a660c5d59003dad975526e7af9cb2e51c5e17
MassimilianoMura <massimilianomura1983@gmail.com>
2015-11-30 18:37:58 +0000

    adds local passport strategy

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,5 +1,6 @@
 exports.render = function (req, res) {
   res.render('index', {
-    title: 'job seeking organiser'
+    title: 'job seeking organiser',
+    user: req.user ? req.user.username : ''
   })
 };
--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,4 +1,5 @@
-var users = require('../../app/controllers/users.server.controller');
+var users = require('../../app/controllers/users.server.controller'),
+    passport = require('passport');
 
 module.exports = function(app) {
   app.route('/users')
@@ -11,4 +12,18 @@ module.exports = function(app) {
     .delete(users.delete);
 
   app.param('userId', users.userByID);
+
+  app.route('/register')
+    .get(users.renderRegister)
+    .post(users.register);
+
+  app.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', {
+      successRedirect: '/',
+      failureRedirect: '/login',
+      failureFlash: true
+    }));
+
+    app.get('/logout', users.logout);
 };
